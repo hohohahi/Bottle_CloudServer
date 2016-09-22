140156adf3ef7f2772599b38ca5004f8984f0eef
Rainman.Yu <Rainman.Yu@Rainman01.EveryMatrix.em>
2016-09-22 07:50:37 +0000

    update

--- deploy_50.sh
+++ deploy_50.sh
@@ -1,6 +1,6 @@
 export JAVA_HOME=/usr/java
 export TOMCAT_HOME=/home/yw/apache-tomcat-6.0.43
-export OM_HOME=/home/yw/KPIMonitorDeploy
+export OM_HOME=/home/yw/Bottle_CloudServerDeploy
 export PATH=$PATH:$TOMCAT_HOME/bin:$JAVA_HOME/bin:
 export M2_HOME=/home/yw/apache-maven-3.2.3
 export M2=$M2_HOME/bin
@@ -13,11 +13,11 @@ rm -r $TOMCAT_HOME/work/*
 pkill -9 java
 mvn clean package
 
-mv target/*.war /home/yw/KPIMonitor.zip
+mv target/*.war /home/yw/Bottle_CloudServer.zip
 rm -rf $OM_HOME
 mkdir $OM_HOME
 mkdir $OM_HOME/web
-unzip /home/yw/KPIMonitor.zip -d $OM_HOME/web
+unzip /home/yw/Bottle_CloudServer.zip -d $OM_HOME/web
 
 nohup startup.sh
 tail -f $TOMCAT_HOME/logs/catalina.out
